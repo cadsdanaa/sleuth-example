35cc7da07e14d2445f3d1dd18c0b61d7114fc800
Alex Dana <cadsdanaa@gmail.com>
2020-09-01 09:56:07 -0500

    Handling server error on go side

--- go/main.go
+++ go/main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/goThing", someHandler)
-	http.ListenAndServe(":8082", nil)
+	err := http.ListenAndServe(":8082", nil)
+	if err != nil {
+		log.Fatal("Failed to listen on port 8082...")
+	}
 }
 
 func someHandler(writer http.ResponseWriter, request *http.Request) {
